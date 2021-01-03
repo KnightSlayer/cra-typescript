bfe9eca12ad4bc7e18981967bfa88cd036f0b321
Abgaryan <garinich.7@gmail.com>
2021-01-03 22:52:12 +0600

    Try react-eouter hooks

--- src/components/routes/Articles/ById/ArticlesById.tsx
+++ src/components/routes/Articles/ById/ArticlesById.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { useParams, useHistory, useLocation, useRouteMatch } from "react-router-dom";
 
-const ArticlesById = (props: any) => {
-  console.log('props', props)
+interface IArticle {
+  foo: string,
+}
+
+
+const ArticlesById = (props: IArticle) => {
+  console.log('props', props);
+  const params: {id: string} = useParams();
+  const history = useHistory();
+  const location = useLocation();
+  const routeMatch = useRouteMatch();
+  console.log('params', params);
+  console.log('history', history);
+  console.log('location', location);
+  console.log('routeMatch', routeMatch);
 
   return (
     <div>
-      By Id
+      By Id: { params.id }
     </div>
   )
 }
--- src/components/routes/Articles/index.tsx
+++ src/components/routes/Articles/index.tsx
@@ -12,7 +12,7 @@ const Routes = () => (
     </Route>
 
     <Route exact path={byIdPath}>
-      <ArticlesById/>
+      <ArticlesById foo="bar"/>
     </Route>
 
     <Route>
