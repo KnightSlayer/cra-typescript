470502430c624fb1b0cb3e6f99823b23cd82460b
Abgaryan <garinich.7@gmail.com>
2021-01-05 13:17:28 +0600

    move redux Provider to App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,8 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "store/store";
 import Home, { path as homePath } from './routes/Home/Home'
 import ArticlesRoutes, { path as articlesPath } from './routes/Articles'
 import Counter, { path as counterPath } from './routes/Counter/Counter'
@@ -13,38 +15,40 @@ import NotFound from './routes/NotFound/NotFound'
 
 function App() {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to={homePath}>Home</Link>
-          </li>
-          <li>
-            <Link to={articlesPath}>Articles</Link>
-          </li>
-          <li>
-            <Link to={counterPath}>Counter</Link>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route path={homePath} exact>
-          <Home/>
-        </Route>
+    <Provider store={store}>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to={homePath}>Home</Link>
+            </li>
+            <li>
+              <Link to={articlesPath}>Articles</Link>
+            </li>
+            <li>
+              <Link to={counterPath}>Counter</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path={homePath} exact>
+            <Home/>
+          </Route>
 
-        <Route path={articlesPath}>
-          <ArticlesRoutes/>
-        </Route>
+          <Route path={articlesPath}>
+            <ArticlesRoutes/>
+          </Route>
 
-        <Route path={counterPath} exact>
-          <Counter/>
-        </Route>
+          <Route path={counterPath} exact>
+            <Counter/>
+          </Route>
 
-        <Route>
-          <NotFound/>
-        </Route>
-      </Switch>
-    </Router>
+          <Route>
+            <NotFound/>
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from "store/store";
 import App from 'components/App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,9 +7,7 @@ import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
